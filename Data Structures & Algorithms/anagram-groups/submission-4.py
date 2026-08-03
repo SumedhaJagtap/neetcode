c2dd58408525af78b8ac451fd11404dc0aa69b81
Sumedha Jagtap <35931017+SumedhaJagtap@users.noreply.github.com>
2026-08-04 01:10:37 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = {}
+
+        for word in strs:
+            # cords = [0]*26
+            # for letter in word:
+            #     cords[ord(letter)-ord('a')]+=1
+            sorted_key = "".join(sorted(word))
+            if sorted_key not in hash_map:
+                hash_map[sorted_key] = []
+            hash_map[sorted_key].append(word)
+        return list(hash_map.values())
